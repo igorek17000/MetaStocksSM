50ae8640ee1af629b951fe6be8295511867cea71
nkt <pauloxti@gmail.com>
2022-06-09 06:31:55 +0100

    refactor

---
+++ contracts/managers/ChainlinkDataFeedsManager.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../interfaces/AggregatorV3Interface.sol";
+
+contract ChainlinkDataFeedsManager {
+    AggregatorV3Interface internal priceFeed;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0x0A77230d17318075983913bC2145DB16C7366156
+        ); // AVAX / USDT
+    }
+
+    /**
+     * Returns the latest price
+     */
+    function getLatestPriceFromChainlink() public view returns (int256) {
+        (
+            ,
+            /*uint80 roundID*/
+            int256 price, /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/
+            ,
+            ,
+
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function getTokensValueInUSD(uint256 _amount, IUniswapV2Router02 dexRouter)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 avaxPrice = uint256(getLatestPriceFromChainlink()) * 1e10;
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = dexRouter.WETH();
+        uint256[] memory amountsOut = dexRouter.getAmountsOut(_amount, path);
+        uint256 tokenAmount = amountsOut[1];
+        return (avaxPrice * tokenAmount) / 1000000000000000000;
+    }
+
+    function getAvaxPriceInUsd() external view returns (uint256) {
+        return uint256(getLatestPriceFromChainlink()) * 1e10;
+    }
+
+    function getAmountOutUSD(uint256 _amount, IUniswapV2Router02 dexRouter)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 avaxPrice = uint256(getLatestPriceFromChainlink()) * 1e10;
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = dexRouter.WETH();
+        uint256[] memory amountsOut = dexRouter.getAmountsOut(_amount, path);
+        uint256 tokenAmount = amountsOut[1];
+
+        return (avaxPrice * tokenAmount) / 1000000000000000000;
+    }
+}
---
+++ contracts/managers/DexRouterManager.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DexRouterManager {
+    IUniswapV2Router02 public dexRouter; // router instance for do swaps
+    address stableCoin;
+
+    constructor(address _dexRouterAddress, address _stableCoin) {
+        dexRouter = IUniswapV2Router02(_dexRouterAddress);
+        stableCoin = _stableCoin;
+    }
+
+    function swapTokensForStableCoin(address to, uint256 amount) public {
+        address[] memory path = new address[](3);
+        path[0] = address(this);
+        path[1] = dexRouter.WETH();
+        path[2] = stableCoin;
+
+        // Do approve for router spend swap token amount
+        IERC20(stableCoin).approve(address(dexRouter), type(uint256).max);
+
+        // swap and transfer to contract
+        dexRouter.swapExactTokensForTokensSupportingFeeOnTransferTokens(
+            amount,
+            0,
+            path,
+            to,
+            block.timestamp + 1000
+        );
+    }
+
+    function swapTokensForBNB(
+        address token,
+        address to,
+        uint256 amount
+    ) public {
+        // generate the uniswap pair path of token -> weth
+        address[] memory path = new address[](2);
+        path[0] = token;
+        path[1] = dexRouter.WETH();
+
+        IERC20(token).approve(address(dexRouter), type(uint256).max);
+
+        // make the swap
+        dexRouter.swapExactTokensForETHSupportingFeeOnTransferTokens(
+            amount,
+            0, // accept any amount of ETH
+            path,
+            to,
+            block.timestamp
+        );
+    }
+
+    /// @notice return the route given the busd addresses and the token
+    function getPathForTokensToTokens(
+        address tokenAddressA,
+        address tokenAddressB
+    ) private pure returns (address[] memory) {
+        address[] memory path = new address[](2);
+        path[0] = tokenAddressA;
+        path[1] = tokenAddressB;
+        return path;
+    }
+
+    function addLiquidity(
+        address token,
+        address to,
+        uint256 tokenAmount,
+        uint256 ethAmount
+    ) private {
+        // approve token transfer to cover all possible scenarios
+        IERC20(token).approve(address(dexRouter), type(uint256).max);
+
+        // add the liquidity
+        dexRouter.addLiquidityETH{value: ethAmount}(
+            address(this),
+            tokenAmount,
+            0, // slippage is unavoidable
+            0, // slippage is unavoidable
+            to, // send lp tokens to owner
+            block.timestamp + 10000
+        );
+    }
+}
