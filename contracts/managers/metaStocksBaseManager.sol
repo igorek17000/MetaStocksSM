fe7340f76ad19ea0dc9036ea6af0bdf4d834a798
nkt <pauloxti@gmail.com>
2022-06-10 07:05:13 +0100

    refactor

--- contracts/managers/metaStocksBaseManager.sol
+++ contracts/managers/metaStocksBaseManager.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "../../interfaces/chainlinkInterfaces/AggregatorV3Interface.sol";
-import "../../interfaces/metaStockInterfaces/IMetaStocksManager.sol";
-import "../../models/TransactionFees.sol";
+import "../interfaces/chainlinkInterfaces/AggregatorV3Interface.sol";
+import "../interfaces/metaStockInterfaces/IMetaStocksManager.sol";
+import "../models/TransactionFees.sol";
 
 abstract contract MetaStocksBaseManager is
     ERC20Upgradeable,
